12bb004bccd9bf08eb36dac495b798c6700b08d2
Risha Asdiyana Rifi <risha@atmatech.net>
2019-12-03 23:06:29 +0700

    push all file

---
+++ application/controllers/AnggotaController.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+//header('Access-Control-Allow-Origin: *');
+
+class AnggotaController extends CI_Controller {
+
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     * 		http://example.com/index.php/welcome
+     *	- or -
+     * 		http://example.com/index.php/welcome/index
+     *	- or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see https://codeigniter.com/user_guide/general/urls.html
+     */
+
+    public function dashboard()
+    {
+        $this->data['content'] = 'menu_admin';
+        $this->load->view('layout/master_layout', $this->data);
+    }
+
+    public function add_donasi()
+    {
+        $this->data['content'] = 'add_donasi';
+        $this->load->view('layout/master_layout', $this->data);
+    }
+}
